c9224ad06f29e05b35078fb62345b19b6cc1025d
AmirAltakroori <ameertakrouri99@gmail.com>
2020-04-29 00:43:38 +0300

    change the access modifiers in MLP

--- models/MLP_model.py
+++ models/MLP_model.py
@@ -3,6 +3,9 @@
 Created on Wed Apr 15 21:07:05 2020
 
 @author: Amir Hashem Altakroori
+
+This file contains the Multi-Layer Perceptron Regressor (MLP Regressor) Class
+
 """
 
 import numpy as np
@@ -15,21 +18,21 @@ class MLP_Regression:
         
         countryData = [x.values for x in countryData]
         
-        self.dateCountry = np.concatenate((countryData[0], countryData[1]))
-        self.valueCountry = np.concatenate((countryData[2], countryData[3]), axis=None)
+        self.__dateCountry = np.concatenate((countryData[0], countryData[1]))
+        self.__valueCountry = np.concatenate((countryData[2], countryData[3]), axis=None)
         
-        self.usingWorld = False
+        self.__usingWorld = False
         if worldData is not None:
             worldData = [x.values for x in worldData]
-            self.usingWorld = True    
+            self.__usingWorld = True    
         
             
-        self.countryModel =  self.bestFit (countryData)           
-        self.worldModel = None
+        self.__countryModel =  self.__bestFit (countryData)           
+        self.__worldModel = None
         if worldData is not None:
-            self.worldModel = self.bestFit (worldData)
+            self.__worldModel = self.__bestFit (worldData)
             
-    def bestFit (self, dataSplit):
+    def __bestFit (self, dataSplit):
         X_train, X_test, y_train, y_test = dataSplit
         mlpreg = MLPRegressor (hidden_layer_sizes = [7],
                                alpha = 0.0001,
@@ -43,29 +46,29 @@ class MLP_Regression:
         
         
     def bestPredect (self, X_test, plot=False):
-        predict = self.countryModel.predict (X_test)
-        if self.usingWorld:
-            predicitWorld = self.worldModel.predict (X_test)
-            predict = self.getApproximation (countryData = predict, worldData = predicitWorld)
+        predict = self.__countryModel.predict (X_test)
+        if self.__usingWorld:
+            predicitWorld = self.__worldModel.predict (X_test)
+            predict = self.__getApproximation (countryData = predict, worldData = predicitWorld)
             
         predict = [abs(int(x)) for x in predict]
         if plot:
-            self.carve (X_test, predict)
+            self.__carve (X_test, predict)
             
         return predict
         
-    def getApproximation (self, countryData, worldData, countryFactor = 0.80, worldFactor = 0.20):
+    def __getApproximation (self, countryData, worldData, countryFactor = 0.80, worldFactor = 0.20):
         
         factoredCountry = [x * countryFactor for x in countryData]
         factoredWorld = [x * worldFactor for x in worldData]
 
         return [int(a + b) for (a, b) in zip (factoredCountry, factoredWorld)]
           
-    def carve(self, X_test, prediction):
+    def __carve(self, X_test, prediction):
         xList = np.arange(1, 61, 1).reshape(-1, 1)
         yList = self.bestPredect (xList)
         plt.scatter(X_test, prediction, color='red', label='test data')
-        plt.scatter(self.dateCountry, self.valueCountry, color='blue', label='train data')
+        plt.scatter(self.__dateCountry, self.__valueCountry, color='blue', label='train data')
         plt.plot(xList, yList, linewidth=3, color="black", label='predictions')
         plt.legend()
         plt.show()
